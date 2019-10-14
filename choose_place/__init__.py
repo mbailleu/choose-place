d93cd5e5fe6e1cdd5d1e711999f1d4efdddb1db2
Jörg Thalheim <joerg@thalheim.io>
2019-10-14 14:59:29 +0100

    use ordered list in places template

--- choose_place/__init__.py
+++ choose_place/__init__.py
@@ -11,17 +11,13 @@ app = Flask(__name__)
 ROOT = Path(os.path.dirname(os.path.realpath(__file__))).joinpath("..")
 
 
-def choose_place(kind: str) -> List[str]:
+def choose_places(kind: str) -> List[str]:
     kind_csv = ROOT.joinpath(kind)
     data = np.genfromtxt(kind_csv, dtype=None, delimiter=",", names=True, encoding=None)
     max_vec = np.vectorize(lambda x: max(0, x))
     data["Preference"] = max_vec(data["Preference"])
     p_v = data["Preference"] / data["Preference"].sum()
-    values = np.random.choice(data["Name"], 3, replace=False, p=p_v)
-    resp = []
-    for i, v in enumerate(values):
-        resp.append("%d: %s" % (i, v))
-    return resp
+    return np.random.choice(data["Name"], 3, replace=False, p=p_v)
 
 
 def create_app():
@@ -38,7 +34,7 @@ def index() -> str:
 def choose_place_html(kind: str) -> str:
     if kind not in ["pub", "lunch", "dinner", "breakfast"]:
         abort(404)
-    places = choose_place(f"{kind}.csv")
+    places = choose_places(f"{kind}.csv")
     return render_template("places.html", places=places)
 
 
@@ -46,7 +42,8 @@ def main() -> None:
     if len(sys.argv) == 1:
         print(sys.argv[0], "list")
         sys.exit(1)
-    print("\n".join(choose_place(sys.argv[1])))
+    for i, place in enumerate(choose_places(sys.argv[1])):
+        print(f"{i}: {place}")
 
 
 if __name__ == "__main__":
