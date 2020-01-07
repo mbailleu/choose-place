34e6a51269dce2203c392bd897f0a836e71b6fe3
Jörg Thalheim <joerg@thalheim.io>
2020-01-07 14:29:29 +0000

    default.nix: remove numpy add gunicorn

--- default.nix
+++ default.nix
@@ -2,7 +2,9 @@ with import <nixpkgs> {};
 python3.pkgs.buildPythonApplication {
   name = "choose-place";
   src = ./.;
-  propagatedBuildInputs = with python3.pkgs; [
-    flask numpy
-  ];
+  propagatedBuildInputs = with python3.pkgs; [ flask ];
+
+  shellHook = ''
+    export PATH=$PATH:${python3.pkgs.gunicorn}/bin
+  '';
 }
